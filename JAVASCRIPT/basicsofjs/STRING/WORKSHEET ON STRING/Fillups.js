4c53aa7ee698144a46b9cc67b7f8f460b0f90291
akshita-1218 <akshitasharma8088@gmailcom>
2025-05-19 23:40:20 +0530

    added

---
+++ JAVASCRIPT/basicsofjs/STRING/WORKSHEET ON STRING/Fillups.js
@@ -0,0 +1,9 @@
+1. Template literals are enclosed by "backticks (``)" instead of quote
+
+2. str.split(" ") will return an array of "words"
+
+3. str.toLowerCase() is used to convert a string to "lowercase"
+
+4. str.replaceAll("a", "z") replaces "all" occurrences of the letter "a"
+
+5. str.startsWith("web") returns a "boolean" value
---
+++ JAVASCRIPT/basicsofjs/STRING/WORKSHEET ON STRING/G.js
@@ -0,0 +1,29 @@
+// q1
+// let str = "JavaScript"
+// let vowels = "aeiou"
+// let count = 0
+// for (let ch of str.toLowerCase()) {
+//   if (vowels.includes(ch)) {
+//     count++;
+//   }
+// }
+// console.log(count)
+// [Running] node "c:\Users\akshi\OneDrive\Desktop\WEB dev cohort\JAVASCRIPT\basicsofjs\STRING\WORKSHEET ON STRING\G.js"
+// 3
+
+// q2
+
+// // q3
+// let str = "madam";
+// let reversed = str.split("").reverse().join("");
+
+// if (str === reversed) {
+//   console.log("It is a palindrome.");
+// } else {
+//   console.log("It is not a palindrome.");
+// }
+// [Running] node "c:\Users\akshi\OneDrive\Desktop\WEB dev cohort\JAVASCRIPT\basicsofjs\STRING\WORKSHEET ON STRING\G.js"
+// It is a palindrome.
+
+
+
